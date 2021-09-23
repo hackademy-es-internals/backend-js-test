cd9d476e8a611959cbbd2148149655df11d5d0f3
Nicola Milella <nicombk@gmail.com>
2021-09-23 16:50:24 +0200

    server configuration and middleware

---
+++ source/server.ts
@@ -0,0 +1,57 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logging from './config/logging';
+import config from './config/config';
+
+//declar namespace
+const NAMESPACE = 'Server'; // what we use to determine where our logs are coming from
+//declare router
+const router = express(); // what defines api behavior
+
+// logging the request
+// inject a middleware for loggig the request
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}] URL - [${req.url}], IP = [${req.socket.remoteAddress}]`);
+    // access the response
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}] URL - [${req.url}], IP = [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`); // statusCode is the status we return depending on the route that user picks
+    });
+
+    next();
+});
+
+// parse the body of the request
+router.use(bodyParser.urlencoded({ extended: false })); // allow us to send nested json to our api
+router.use(bodyParser.json()); // allow us to not have to call json.parse or json.stringify (react)
+
+// define the rules of our api
+router.use((req, res, next) => {
+    res.header('Access-Controle-Allow-Origin', '*'); // only in local development, in production better use ip predefined
+    res.header('Access-Controle-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        // OPTION???
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+// define routes
+
+// error handling
+router.use((req, res, next) => {
+    const error = new Error('not found');
+
+    return res.status(404).json({
+        message: error.message
+    });
+
+    next();
+});
+
+// create the server
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
