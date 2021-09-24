92e22bfcb85e62029836ef25930a5f0377f4bc63
Nicola Milella <nicombk@gmail.com>
2021-09-24 12:46:40 +0200

    comments to model

--- source/models/book.ts
+++ source/models/book.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 import IBook from '../interfaces/book';
 
+// constant BookSchema of type Schema
 const BookSchema: Schema = new Schema(
     {
         title: { type: String, required: true },
@@ -8,8 +9,11 @@ const BookSchema: Schema = new Schema(
         extraInformation: { type: String }
     },
     {
-        timestamps: true
+        timestamps: true // created_at and updated_at
     }
 );
+// this means that whene you're creating a new book document it has to have title and author because are required
 
+// exportin this model in order to use it in our api
+// we pass <IBook> because everytime we use a mongoose function like find, updateById, deleteOne whatever is being returned in then block it's going to know to use IBook interface, therefore we're going to have access to all of our variables
 export default mongoose.model<IBook>('Book', BookSchema);
