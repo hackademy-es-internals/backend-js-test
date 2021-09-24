a2ed3207e44481c9c202c3e6aa8f9b8d209265e9
Nicola Milella <nicombk@gmail.com>
2021-09-24 13:01:08 +0200

    read and write book

--- source/controllers/books.ts
+++ source/controllers/books.ts
@@ -1,13 +1,49 @@
 import { Request, Response, NextFunction } from 'express';
 import logging from '../config/logging';
+import Book from '../models/book';
+import mongoose from 'mongoose';
 
-const NAMESPACE = 'Sample Controller';
+const NAMESPACE = 'Books Controller';
 
-const getAllBooks = (req: Request, res: Response, next: NextFunction) => {
-    logging.info(NAMESPACE, `Sample health check route called.`);
-    return res.status(200).json({
-        message: 'pong'
+const createBook = (req: Request, res: Response, next: NextFunction) => {
+    let { author, title } = req.body;
+
+    const book = new Book({
+        _id: new mongoose.Types.ObjectId(),
+        author,
+        title
     });
+
+    return book
+        .save()
+        .then((result) => {
+            return res.status(201).json({
+                book: result
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
 };
 
-export default { getAllBooks };
+const getAllBooks = (req: Request, res: Response, next: NextFunction) => {
+    Book.find()
+        .exec()
+        .then((results) => {
+            return res.status(200).json({
+                books: results,
+                count: results.length
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
+};
+
+export default { getAllBooks, createBook };
--- source/routes/book.ts
+++ source/routes/book.ts
@@ -4,5 +4,6 @@ import controller from '../controllers/books';
 const router = express.Router();
 
 router.get('/get/books', controller.getAllBooks);
+router.post('/create/book', controller.createBook);
 
 export = router;
