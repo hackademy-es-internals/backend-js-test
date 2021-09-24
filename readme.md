e91b503f9500d2b85a48e36384701919bb11afc0
Nicola Milella <nicombk@gmail.com>
2021-09-24 20:44:00 +0200

    save post operation on Book model

---
+++ readme.md
@@ -0,0 +1,7 @@
+npm init  
+npm install -g typescript nodemon ts-node prettier tsc --init  
+npm install express body-parser dotenv npm install --save-dev @types/express @types/body-parser @types/dotenv
+
+nodemon source/server.ts npm run build
+
+npm install mongoose npm install --save-dev @types/mongoose
--- source/models/book.ts
+++ source/models/book.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import IBook from '../interfaces/book';
+import logging from '../config/logging';
 
 // constant BookSchema of type Schema
 const BookSchema: Schema = new Schema(
@@ -13,6 +14,11 @@ const BookSchema: Schema = new Schema(
     }
 );
 // this means that whene you're creating a new book document it has to have title and author because are required
+
+// example of pre/post save operation
+BookSchema.post<IBook>('save', function () {
+    logging.info('Mongo', 'Checkout the book we just saved: ', this);
+});
 
 // exportin this model in order to use it in our api
 // we pass <IBook> because everytime we use a mongoose function like find, updateById, deleteOne whatever is being returned in then block it's going to know to use IBook interface, therefore we're going to have access to all of our variables
