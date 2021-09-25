bf87661c3cc3c50dcb3eb0a8b53b49cb88e1a14f
Nicola Milella <nicombk@gmail.com>
2021-09-25 11:34:45 +0200

    function signJWT

---
+++ source/functions/signJWT.ts
@@ -0,0 +1,42 @@
+import e from 'express';
+import jwt from 'jsonwebtoken';
+import config from '../config/config';
+import logging from '../config/logging';
+import IUser from '../interfaces/user';
+// create the token and return it to users in order to use it for the following requests to protected routes
+
+const NAMESPACE = 'Auth';
+
+const signJWT = (user: IUser, callback: (error: Error | null, token: string | null) => void): void => {
+    var timeSinchEpoch = new Date().getTime();
+    var expirationTime = timeSinchEpoch + Number(config.server.token.expireTime) * 100000;
+    var expirationTimeInSeconds = Math.floor(expirationTime / 1000);
+
+    logging.info(NAMESPACE, `Attempting to sign token for ${user.username}`);
+
+    try {
+        jwt.sign(
+            {
+                username: user.username
+            },
+            config.server.token.secret,
+            {
+                issuer: config.server.token.issuer,
+                algorithm: 'HS256',
+                expiresIn: expirationTimeInSeconds
+            },
+            (error, token) => {
+                if (error) {
+                    callback(error, null);
+                } else if (token) {
+                    callback(null, token);
+                }
+            }
+        );
+    } catch (error) {
+        logging.error(NAMESPACE, error.message, error);
+        callback(error, null);
+    }
+};
+
+export default signJWT;
